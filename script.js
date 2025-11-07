da68836819c93de6279419a7715e22062e0c02b9
JuanitaJabon <j.bustosrey@sanvicente.redfasta.edu.ar>
2025-11-07 08:00:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function startNow() {
+  window.location.href = "https://www.canva.com/design/DAG2s6bvVtc/OiU42BAjXPGzolMAlq73Ng/edit?utm_content=DAG2s6bvVtc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"; // Reemplaza con tu enlace real
+}
